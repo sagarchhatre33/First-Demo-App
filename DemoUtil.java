67b22f3da7df48986be881d37f86bfeb1b52a910
sagar29chhatre <71561236+sagar29chhatre@users.noreply.github.com>
2021-01-27 09:20:17 -0800

    files edited

--- DemoUtil.java
+++ DemoUtil.java
@@ -1,7 +1,6 @@
 public class Demo{
 
-	public static void main(String[] args){
-	
-	
+	void m1(){
+		System.out.println("m1 method");
 	}
 }
--- Test.java
+++ Test.java
@@ -1,7 +1,4 @@
 public class Demo{
 
-	public static void main(String[] args){
-	
-	
-	}
+	int a = 10;
 }
